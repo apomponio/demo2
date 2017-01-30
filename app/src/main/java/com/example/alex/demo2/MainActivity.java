6c973f950fd8b91b1a1c5b8a372bfdc69184ccc6
Alex <pomponioalex@gmail.com>
2017-01-30 01:53:46 -0500

    Test commit

---
+++ app/src/main/java/com/example/alex/demo2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.alex.demo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void mOnClick(View view) {
+        ((Button)view).setText("Clicked " + (++count));
+
+    }
+}
